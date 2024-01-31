e7e76fb60e7211a479252e297fb07e000978ce27
Dhara <dhara00719@gmail.com>
2024-01-31 19:07:00 +0400

    add design

--- app/src/main/java/com/indie/apps/pannypal/HomeActivity.java
+++ app/src/main/java/com/indie/apps/pannypal/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity
@@ -12,6 +13,8 @@ public class HomeActivity extends AppCompatActivity
 
     ImageButton imgbtnContact, imgbtnHome, imgbtnCalculator;
     ImageButton imgbtnProfile;
+
+    Button btnNewEntry;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +29,7 @@ public class HomeActivity extends AppCompatActivity
         imgbtnCalculator.setSelected(false);
 
         imgbtnProfile = findViewById(R.id.imgbtnProfile);
+        btnNewEntry = findViewById(R.id.btnNewEntry);
 
 
         imgbtnContact.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +51,14 @@ public class HomeActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(HomeActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnNewEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(HomeActivity.this, ContactEntryActivity.class);
                 startActivity(i);
             }
         });
